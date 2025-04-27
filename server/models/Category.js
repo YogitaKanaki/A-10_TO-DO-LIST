3e32bb326786d49387f847d77b26eed1dd6b6a4f
Radhagit <radhadesh17@gmail.com>
2025-04-27 16:02:05 +0530

    backend is ready

---
+++ server/models/Category.js
@@ -0,0 +1,16 @@
+// models/Category.js
+
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userEmail: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Category', categorySchema);
